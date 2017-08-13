bae3755f54e500853bad6aa87c7db0d4e5bd6c9a
gafker <gafker@sina.com>
2017-08-13 14:59:05 +0000

    commo

--- gafker.manage/gafker-manage-web/src/main/java/com/gafker/manage/task/PhoneSevenTask.java
+++ gafker.manage/gafker-manage-web/src/main/java/com/gafker/manage/task/PhoneSevenTask.java
@@ -1,16 +1,21 @@
 package com.gafker.manage.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
-//@Component
+import com.gafker.manage.service.PhonesevenService;
+import com.gafker.manage.service.utils.threads.GetGeoPosition;
+
+@Component
 public class PhoneSevenTask {
-//	@Autowired
-//	private ThreadPoolTaskExecutor taskExecutor;
-//	@Autowired
-//	private PhonesevenService phonesevenService;
-//	//@Scheduled(cron = "0 0/20 * * * ?")  
-//	public void phoneSevenGeo(){
-//			taskExecutor.execute(new GetGeoPosition(phonesevenService));
-//	}
+	@Autowired
+	private ThreadPoolTaskExecutor taskExecutor;
+	@Autowired
+	private PhonesevenService phonesevenService;
+	@Scheduled(cron = "0 0/5 * * * ?")  
+	public void phoneSevenGeo(){
+			taskExecutor.execute(new GetGeoPosition(phonesevenService));
+	}
 }
